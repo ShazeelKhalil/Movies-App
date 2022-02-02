27589aca1c5ae3ae597c2a7d6bea1a81b62800ce
ShazeelKhalil <shazeelkhalil@gmail.com>
2022-02-02 10:49:55 +0500

    first movies app commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React,  {useEffect , useState} from 'react';
+import './App.css';
+import Movie from './components/Movie'
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+//const IMAGE_API = "https://image.tmbd.org/t/p/w1280"
+
+function App() {
+  const [movies , setMovies] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  useEffect(() => {
+      fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+          console.log(data);
+          setMovies(data.results);
+      });
+
+  }, []);
+
+  const handleOnSubmit =(e) => {
+    e.preventDefault(); 
+
+    fetch(SEARCH_API + searchTerm)
+      
+      .then((res) =>
+      {
+        console.log(res)
+      return res.json()
+    }
+      )
+      .then((data) => {
+          console.log(data);
+          setMovies(data.results);
+       });
+
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+
+return ( 
+  <>
+  <header>
+    <form onSubmit={handleOnSubmit}>
+    <input className='search' type="search" placeholder='Search...' value={searchTerm} onChange={handleOnChange } />
+
+    </form>
+  </header>
+  
+  <div className='movie-container' > 
+  
+  
+  {movies.length > 0 && movies.map((movie) => 
+  <Movie key = {movie.id} {...movie} />
+  )}
+    
+    </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,35 @@
+import React  from 'react';
+
+//const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280"
+
+
+
+const Movie = ({title, poster_path, overview, vote_average}) => {
+    return(
+    <div className='movie'>
+        <img src={IMG_API + poster_path} alt={title} />
+        <div className='movie-info'>
+            <h3>
+                {title}
+            </h3>
+            <span>
+                {vote_average}
+            </span>
+        </div>
+        <div className='movie-over'>
+            <h2>Overview</h2>
+            <p>
+                {overview}
+            </p>
+        </div>
+
+        
+    </div>
+    )
+  
+};
+
+export default Movie;
+
